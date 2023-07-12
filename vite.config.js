66655f66ec93d6b2ba7649a3f16a9231ff93ff18
Fahad Devnikar <devnikarfahad@gmail.com>
2023-07-12 07:00:37 +0530

    Updated Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewares: [
+      compression({ level: 5 }), // Add this line
+    ],
+  },
 })
